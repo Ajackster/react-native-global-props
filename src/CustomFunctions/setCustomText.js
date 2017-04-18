2da06558e34e7deadd560c20dbe11657eb326a63
Kevin <kevin.nalinne@gmail.com>
2017-04-18 11:34:12 +0200

    Override this.props with custom props if prop value is equal to default prop

--- src/CustomFunctions/setCustomText.js
+++ src/CustomFunctions/setCustomText.js
@@ -4,9 +4,17 @@ import {
 
 export const setCustomText = customProps => {
   const textRender = Text.prototype.render;
+  const defaultProps = Text.defaultProps;
   Text.prototype.render = function render() {
     let oldProps = this.props;
-    this.props = { ...customProps, ...this.props, style: [customProps.style, this.props.style] };
+    let defaultPropsOverride = {};
+    Object.entries(customProps).forEach( entry => {
+      const [k, v] = entry;
+      if(oldProps[k] === defaultProps[k]) {
+        defaultPropsOverride[k] = v;
+      }
+    });
+    this.props = { ...customProps, ...this.props, ...defaultPropsOverride, style: [customProps.style, this.props.style] };
     try {
       return textRender.apply(this, arguments);
     } finally {
